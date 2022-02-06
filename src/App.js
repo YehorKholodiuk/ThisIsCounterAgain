f2fdcc8453b2dad4e71d6b1266ddedbc4ce72822
ASUS <Yehor Kholodiuk>
2022-02-06 12:21:52 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import React from "react";
+
 function App() {
     const [counters, setCounters] = useState([
         {id: 1, counter: 5},
@@ -33,41 +34,60 @@ function App() {
         // };
     };
     const Reset = (id) => {
-        const updatedCounters = counters.map(el => el.id === id ? {...el, counter:0}:el);
+        const updatedCounters = counters.map(el => el.id === id ? {...el, counter: 0} : el);
         setCounters(updatedCounters)
     }
     const addCounter = () => {
-        const newCounter = {id: Math.random(),counter:0}
+        const newCounter = {id: Math.random(), counter: 0}
         setCounters([...counters, newCounter])
     }
 
     const DeleteThis = (id) => {
- setCounters([...counters].filter(el => el.id !== id))
+        setCounters([...counters].filter(el => el.id !== id))
 
     }
 
-    const setForAll = ( ) => {
+    const setForAll = () => {
         setCounters([...counters].map(el => ({...el, counter: inputNum})));
     }
 
+    const up = (id) => {
+        const index = counters.findIndex(el => el.id === id);
+        const result = [...counters.slice(0, index - 1), counters[index], counters[index - 1], ...counters.slice(index + 1)]
+        setCounters(result)
+    }
+
+    const down = (id) => {
+        const index = counters.findIndex(el => el.id === id);
+        const result = [...counters.slice(0, index), counters[index + 1], counters[index], ...counters.slice(index + 2)]
+        setCounters(result)
+    }
+
+
     return (
         <div>
-            {counters.map(el => <div key={el.id}>
-                <button onClick={() => minus(el.id)}>Minus</button>
-                {el.counter}
-                <button onClick={() => plus(el.id)}>Plus</button>
-                <button onClick={() => Reset(el.id)}>reset</button>
-                <button onClick={() => DeleteThis(el.id)}> Delete it</button>
-            </div>)}
+            {counters.map((el, index) =>
+                <div key={el.id}>
+                    <button onClick={() => minus(el.id)}>Minus</button>
+                    {el.counter}
+                    <button onClick={() => plus(el.id)}>Plus</button>
+                    <button onClick={() => Reset(el.id)}>reset</button>
+                    <button onClick={() => DeleteThis(el.id)}> Delete it</button>
+                    {index !== 0 ? <button onClick={() => up(el.id)}>Up</button> : null}
+                    {index !== counters.length - 1 ? <button onClick={() => down(el.id)}>Down</button> : null}
+
+                </div>)}
             <div>
-                <input  type="text" placeholder='enter any number' value={inputNum}
-                onChange = {(event) => setInput(Number(event.target.value))}
+                <input type="text" placeholder='enter any number' value={inputNum}
+                       onChange={(event) => setInput(Number(event.target.value))}
                 />
-                    <button onClick={setForAll}>Set For all</button>
-                    <input type="text" onChange={(event) => console.log(event)}/>
+                <button onClick={setForAll}>Set For all</button>
+                <input type="text" onChange={(event) => console.log(event)}/>
             </div>
             <hr/>
             <button onClick={addCounter}>Add Counter</button>
+
+
         </div>
     );
 }
